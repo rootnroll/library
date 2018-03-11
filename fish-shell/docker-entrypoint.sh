155357cc994ad42385e2e4892144fbd0e8810430
Pavel Sviderski <pavel@psviderski.name>
2018-03-11 22:39:54 +0300

    add fish-shell image

---
+++ fish-shell/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Print the greeting
+cat /.greeting 2> /dev/null
+
+while true; do
+    /usr/bin/fish
+done
---
+++ fish-shell/fish_init.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+
+# Set fish default color theme
+set -U fish_color_autosuggestion 555
+set -U fish_color_command 005fd7
+set -U fish_color_comment 990000
+set -U fish_color_end 009900
+set -U fish_color_error ff0000
+set -U fish_color_param 00afff
+set -U fish_color_quote 999900
+set -U fish_color_redirection 00afff
