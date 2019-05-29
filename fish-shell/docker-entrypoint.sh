ab7eb56efdc2cbc22ad8066153076cd0945ba057
Pavel Sviderski <me@psviderski.name>
2019-05-29 21:16:39 +1000

    update fish to 3.0.2 in fish image, rebase on rootnroll/demo-ubuntu:18.04

--- fish-shell/docker-entrypoint.sh
+++ fish-shell/docker-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-/opt/fish_init.fish
-
 # Print the greeting
 cat /.greeting 2> /dev/null
 
 while command -v /usr/bin/fish >/dev/null; do  # a user may delete the executable
-    /usr/bin/fish
+    gosu box /usr/bin/fish -l
 done
